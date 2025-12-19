3fa3595ab53f93684d1c86b1a8c58ee35cdd1458
Mayar Ahmed <mma121@pitt.edu>
2025-12-19 08:46:08 -0500

    adding day 3 of advent of code 2025

---
+++ day_3/day_3.py
@@ -0,0 +1,49 @@
+from collections import deque
+import time
+
+start_time: float=time.time()
+file="input_d3_25.txt"
+#file="tmp.txt"
+
+with open(file,"r",encoding="utf8") as f:
+    data=f.readlines()
+    
+def find_combinations(line:str)->int:
+    combinations=set()
+    length=len(line)
+    for i in range(length):
+        for j in range(i+1,length):
+            value=str(line[i])+str(line[j])
+            combinations.add(value)
+            
+    val_typ= [int(x) for x in combinations]
+    return max(val_typ)
+                
+def find_max_subseq(line:str,k:int=12)->int:
+    length=len(line)
+    remove=length-k
+    stack=deque()
+    
+    for ch in line:
+        while remove>0 and len(stack)>0 and stack[-1]<ch:
+            stack.pop()
+            remove-=1
+        stack.append(ch)
+    #if after the loop we still have to remove some digits, remove them from the end
+    if remove > 0:
+        stack = list(stack)[:-remove]
+    vale = "".join(list(stack)[:k])
+    return int(vale)
+
+sum=0   
+second_sum=0     
+for line in data:
+    sum+=find_combinations(line.strip())
+    #sum+=find_max_subseq(line.strip(),2)
+    second_sum+=find_max_subseq(line.strip(),12)
+    
+
+end_time=time.time()
+print("The sum of the highest two digits in each line is:",sum)
+print("The sum of the highest 12 digit combinations in each line is:",second_sum)
+print("Time taken:",end_time-start_time)
